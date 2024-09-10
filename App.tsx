1540e282a3841b787113d5911c6b800010f80385
machu2333 <973830762@qq.com>
2024-09-10 17:55:12 +0800

    main2

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {title}mian1
+        {title}mian1mian2
       </Text>
       <Text
         style={[
